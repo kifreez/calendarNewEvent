49c6aae53741b4775a3809639000d0b5f8ff7d59
Anton Kriat <kifreez@gmail.com>
2016-12-30 20:44:43 +0200

    Initial Commit

---
+++ Calendar_test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Calendar_test
+//
+//  Created by Kif on 30.12.16.
+//  Copyright © 2016 KifApp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Calendar_test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Calendar_test
+//
+//  Created by Kif on 30.12.16.
+//  Copyright © 2016 KifApp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
