29afa03141330c7fc5515f6351b9f69478e54ae1
Anton Kriat <kifreez@gmail.com>
2016-12-30 23:13:31 +0200

    added new root view controller

---
+++ Calendar_test/AddEventViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddEventViewController.h
+//  Calendar_test
+//
+//  Created by Kif on 30.12.16.
+//  Copyright © 2016 KifApp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddEventViewController : UITableViewController
+
+@end
